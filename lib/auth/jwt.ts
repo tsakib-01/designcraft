3bc9fc74496ed7b703e7d417c03c7379e4578e76
tsakib-01 <noerrorsyntax@gmail.com>
2026-02-26 23:59:20 +0600

    fix: again

--- lib/auth/jwt.ts
+++ lib/auth/jwt.ts
@@ -1,8 +1,8 @@
 import jwt from 'jsonwebtoken';
 import { AuthTokenPayload } from '@/types/user.types';
 
-const ACCESS_SECRET  = process.env.JWT_ACCESS_SECRET!;
-const REFRESH_SECRET = process.env.JWT_REFRESH_SECRET!;
+const ACCESS_SECRET  = 'abc8e2fd68a9605e8c50fbc356fe6a8472d4182bb6f79a95b7888424aa00f883eafd0eae2ed05f57b2368460b0a76fabbe8e10c94e33f32e910fdaff700951e6'!;
+const REFRESH_SECRET = '71bb5857af9dc8835ddf72abf5316e1e27d54ff29130e44563a41b03d0ed9ab7a351243e4b778bba272da2328448a19a2ee38cf4e672dba3e63d7b4770846ce9';
 
 export const signAccessToken = (payload: AuthTokenPayload): string =>
   jwt.sign(payload, ACCESS_SECRET, { expiresIn: '15m' });
